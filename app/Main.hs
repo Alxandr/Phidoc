711b294cd8570881c5c078a60c7a5e97de5278d3
Alxandr <alxandr@alxandr.me>
2017-01-10 19:06:12 +0100

    Use local GHC

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+import Text.Phidoc
+
+toIO :: Show a => Either a b -> IO b
+toIO (Left e) = fail (show e)
+toIO (Right r) = return r
+
+relinkIO :: [String] -> String -> IO String
+relinkIO path content = toIO (relink path content)
+
+someFunc :: IO ()
+someFunc = do
+  out <- relinkIO [] "Hello from someFun"
+  putStrLn out
 
 main :: IO ()
 main = someFunc
